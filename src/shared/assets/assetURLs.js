5dbb4b17b1f776175be540392aca06fbdb064c42
borkjt9 <borkjt9@gmail.com>
2018-06-11 14:35:50 -0400

    Added preloadIMGs function to App component

--- src/shared/assets/assetURLs.js
+++ src/shared/assets/assetURLs.js
@@ -1,4 +1,17 @@
+import React from 'react';
+
 export const downArrowSrc = 'https://firebasestorage.googleapis.com/v0/b/kaplan-assignment.appspot.com/o/down-arrow.svg?alt=media&token=730f6ee2-1a50-4c95-bbcc-5bba3076ab68';
 export const horArrowSrc = 'https://firebasestorage.googleapis.com/v0/b/kaplan-assignment.appspot.com/o/horizontal-arrows.svg?alt=media&token=c4ea3d7b-5327-429b-9909-3b26f946c24e';
 export const upArrowSrc = 'https://firebasestorage.googleapis.com/v0/b/kaplan-assignment.appspot.com/o/up-arrow.svg?alt=media&token=981a6af0-f37d-44b8-aa11-e734a8c1bec6';
 export const dragPreviewSrc = 'https://firebasestorage.googleapis.com/v0/b/kaplan-assignment.appspot.com/o/preview-img.svg?alt=media&token=dbb729b0-6697-4e68-9498-bbb7847bb029';
+
+const preloadIMGs = () => (
+  <div style={{ display: 'None' }} className="image-loader">
+    <img alt="blurred preview when you drag a answer across screen" src={dragPreviewSrc} />
+    <img alt="horizontal arrows" src={horArrowSrc} />
+    <img alt="down arrow"src={downArrowSrc} />
+    <img alt="up arrow" src={upArrowSrc} />
+  </div>
+);
+
+export default preloadIMGs;
