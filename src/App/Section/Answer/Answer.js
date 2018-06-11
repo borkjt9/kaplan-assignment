7dca350e6323e075d7bf7ef0cef87a3560bf40b6
borkjt9 <borkjt9@gmail.com>
2018-06-11 18:07:21 -0400

    Documentation for Answer & Placeholder component

--- src/App/Section/Answer/Answer.js
+++ src/App/Section/Answer/Answer.js
@@ -5,7 +5,15 @@ import { dragPreviewSrc, horArrowSrc, upArrowSrc, downArrowSrc } from '../../../
 import { DRAGGABLE_TYPE_ANSWER } from '../../../redux/constants';
 import './_Answer.scss';
 
+/**
+ * Used as a parameter to DragSource. Which is a higher order component from react-dnd library.
+ * http://react-dnd.github.io/react-dnd/docs-overview.html
+ */
 const answerSource = {
+  /**
+   * Tells DragSource to begin drag.
+   * @param {Object} props - The components props, plus added props from react-dnd.
+   */
   beginDrag(props) {
     const {
       id,
@@ -14,11 +22,19 @@ const answerSource = {
       side,
       index,
     } = props;
+
     props.setActiveAnswer({ side, index });
     return { id, left, top };
   },
 };
 
+/**
+ * From react-dnd documentation:
+ * For each component that needs to track the drag and drop state,
+ * You can define a collecting function that retrieves the relevant bits of it from the monitors.
+ * React DnD then takes care of timely calling your collecting function
+ * and merging its return value into your components' props.
+ */
 function collect(connect, monitor) {
   return {
     connectDragSource: connect.dragSource(),
@@ -28,19 +44,26 @@ function collect(connect, monitor) {
 }
 
 class Answer extends Component {
-  static propTypes = {
-    connectDragSource: PropTypes.func.isRequired,
-    connectDragPreview: PropTypes.func.isRequired,
-    isDragging: PropTypes.bool.isRequired,
-  }
+  /**
+   * Answers are rendered in locations whenever an answer is not.
+   * Connected to react-dnd higher order component 'DropTarget'.
+   */
 
   componentDidMount() {
+    // Supplies img to be used in drag action.
     const img = new Image();
     img.src = dragPreviewSrc;
     img.style.opacity = 0.5;
     this.props.connectDragPreview(img);
   }
 
+  /**
+   * Called when either of the arrow buttons are clicked.
+   *
+   * @param {Number} toIndex - New location of answer.
+   * @param {Number} fromIndex - Old location of answer.
+   * @param {String} dir - Either 'hor' or 'vert'. used to set toSide and FromSide.
+   */
   callPositionAnswer(toIndex, fromIndex, dir) {
     const { positionAnswer, activeQuestion, side } = this.props;
     const data = {
@@ -64,6 +87,11 @@ class Answer extends Component {
     positionAnswer(data);
   }
 
+  /**
+   * Called when either of the arrow buttons are clicked.
+   *
+   * @param {Number} index - Used to disable up and down buttons if at bottom or top of answers.
+   */
   renderArrowButtons(index) {
     const { side, totalAnswers } = this.props;
     const btnHorizClassName = `answer__arrow-btn --hor --${side}`;
@@ -76,54 +104,52 @@ class Answer extends Component {
         />
       </button>
     );
+    if (side === 'left') {
+      return (
+        horizBtn()
+      );
+    }
     let btnUpClassName = `answer__arrow-btns__arrow-btn --vert --up --${side}`;
-    let btnDownClassName = `answer__arrow-btns__arrow-btn --vert --down --${side}`;
     if (index === 0) {
       btnUpClassName += ' --is-disabled';
     }
+    const upBtn = () => (
+      <button
+        onClick={() => this.callPositionAnswer(index - 1, index, 'vert')}
+        className={btnUpClassName}
+      >
+        <img
+          className="answer__arrow-btns__arrow-btn__up__img"
+          alt="move answers to left side or right side"
+          src={upArrowSrc}
+        />
+      </button>
+    );
 
+    let btnDownClassName = `answer__arrow-btns__arrow-btn --vert --down --${side}`;
     if (index === totalAnswers - 1) {
       btnDownClassName += ' --is-disabled';
     }
-
-    if (side === 'right') {
-      const upBtn = () => (
-        <button
-          onClick={() => this.callPositionAnswer(index - 1, index, 'vert')}
-          className={btnUpClassName}
-        >
-          <img
-            className="answer__arrow-btns__arrow-btn__up__img"
-            alt="move answers to left side or right side"
-            src={upArrowSrc}
-          />
-        </button>
-      );
-
-      const downBtn = () => (
-        <button
-          onClick={() => this.callPositionAnswer(index + 1, index, 'vert')}
-          className={btnDownClassName}
-        >
-          <img
-            className="answer__arrow-btns__arrow-btn__down__img"
-            alt="move answers to left side or right side"
-            src={downArrowSrc}
-          />
-        </button>
-      );
-      return (
-        <div>
-          {horizBtn()}
-          <div className="answer__arrow-btns --vert">
-            {upBtn()}
-            {downBtn()}
-          </div>
-        </div>
-      );
-    }
+    const downBtn = () => (
+      <button
+        onClick={() => this.callPositionAnswer(index + 1, index, 'vert')}
+        className={btnDownClassName}
+      >
+        <img
+          className="answer__arrow-btns__arrow-btn__down__img"
+          alt="move answers to left side or right side"
+          src={downArrowSrc}
+        />
+      </button>
+    );
     return (
-      horizBtn()
+      <div>
+        {horizBtn()}
+        <div className="answer__arrow-btns --vert">
+          {upBtn()}
+          {downBtn()}
+        </div>
+      </div>
     );
   }
 
@@ -142,26 +168,51 @@ class Answer extends Component {
       answerBtnClassName += ' --active';
     }
 
-    return connectDragSource(<div
-      action={this.action}
-      className="answer my-4"
-    >
-      <button onClick={() => toggleArrowButtons(index)} className={isDragging ? `${answerBtnClassName}  --active` : answerBtnClassName}>
-        <p className="m-2">{answersDict[index]}</p>
-      </button>
-      { (activeAnswer.index === index && activeAnswer.side === side) ? this.renderArrowButtons(index) : ''}
-    </div>);
+    //connectDragSource is react-dnd function.
+    return connectDragSource(
+      <div
+        action={this.action}
+        className="answer my-4"
+      >
+        <button onClick={() => toggleArrowButtons(index)} className={isDragging ? `${answerBtnClassName}  --active` : answerBtnClassName}>
+          <p className="m-2">{answersDict[index]}</p>
+        </button>
+        { (activeAnswer.index === index && activeAnswer.side === side) ? this.renderArrowButtons(index) : ''}
+      </div>
+    );
   }
 }
 
 Answer.propTypes = {
+  /** Supplied by react-dnd. */
+  connectDragSource: PropTypes.func.isRequired,
+  /** Supplied by react-dnd. Sets position of hover item. */
+  connectDragPreview: PropTypes.func.isRequired,
+  /** Supplied by react-dnd. True if drag item is being dragged. */
+  isDragging: PropTypes.bool.isRequired,
+  /** activates / deactives answer. All makes arrow buttons visible. */
   toggleArrowButtons: PropTypes.func.isRequired,
+  /**
+   * Current active answer. Supplied to toggleArrowButtons.
+   * EXAMPLE: {side: 'left', index: 2}
+   */
   activeAnswer: PropTypes.objectOf(PropTypes.string).isRequired,
+  /** Side answer is found on. */
   side: PropTypes.string.isRequired,
+  /** current index of answer. */
   index: PropTypes.number.isRequired,
+  /**
+   * Total answers for the question.
+   * Used to render total number of answers and placeholders.
+   */
   answersDict: PropTypes.objectOf(PropTypes.any).isRequired,
+  /**
+   * Total answers for the question.
+   */
   totalAnswers: PropTypes.number.isRequired,
+  /** Changes location of answer in section. */
   positionAnswer: PropTypes.func.isRequired,
+  /** Active question number. */
   activeQuestion: PropTypes.number.isRequired,
 };
 
--- src/App/Section/Placeholder/Placeholder.js
+++ src/App/Section/Placeholder/Placeholder.js
@@ -4,39 +4,65 @@ import { DropTarget } from 'react-dnd';
 import { DRAGGABLE_TYPE_ANSWER } from '../../../redux/constants';
 import './_Placeholder.scss';
 
-function callPositionAnswer(fromIndex, toIndex, positionAnswer, activeQuestion, fromSide, toSide) {
-  const data = {
-    toIndex,
-    fromIndex,
-    activeQuestion,
-  };
-  data.toSide = toSide;
-  data.fromSide = fromSide;
-  positionAnswer(data);
+/**
+ * Called in drop below after an answer is 'dropped' in the placeholder.
+ *
+ * @param {function} positionAnswer - Function fed from App component. Connects to redux store.
+ * @param {Object} positionData - Object used to position element. Contains the below keys:
+ * @key {Number} fromIndex - Old location of answer.
+ * @key {Number} toIndex - New location of answer.
+ * @key {Number} activeQuestion - Index of active question.
+ * @key {Number} fromSide - Old side. Either 'left' or 'right'.
+ * @key {Number} toSide - New side. Either 'left' or right'.
+ */
+function callPositionAnswer(positionAnswer, positionData) {
+  positionAnswer(positionData);
 }
 
+/**
+ * Used as a parameter to DropTarget. Which is a higher order component from react-dnd library.
+ * http://react-dnd.github.io/react-dnd/docs-overview.html
+ */
 const placeholderTarget = {
-
-  canDrop(props) {
+  /**
+   * Returns true because all placeholders are targets
+   * @param {Object} props - The components props, plus added props from react-dnd.
+   */
+  canDrop() {
     return true;
   },
 
+  /**
+   * Is called when a component is 'dropped'.
+   */
   drop(props) {
-    // const {movePiece, position: {x, y}} = props
-    // movePiece(x, y)
     const {
       fromIndex,
       toIndex,
-      positionAnswer,
       activeQuestion,
       fromSide,
       toSide,
+      positionAnswer,
     } = props;
-    callPositionAnswer(fromIndex, toIndex, positionAnswer, activeQuestion, fromSide, toSide);
+
+    const positionData = {
+      toIndex,
+      fromIndex,
+      activeQuestion,
+      toSide,
+      fromSide,
+    };
+    callPositionAnswer(positionAnswer, positionData);
   },
 };
 
-
+/**
+ * From react-dnd documentation:
+ * For each component that needs to track the drag and drop state,
+ * You can define a collecting function that retrieves the relevant bits of it from the monitors.
+ * React DnD then takes care of timely calling your collecting function
+ * and merging its return value into your components' props.
+ */
 function collect(connect, monitor) {
   const info = {
     connectDropTarget: connect.dropTarget(),
@@ -46,6 +72,11 @@ function collect(connect, monitor) {
   return info;
 }
 
+/**
+ * Renders overlay on top of placeholder if dragged answer is hovering over it.
+ *
+ * @param {String} color Color of the overlay.
+ */
 function renderOverlay(color) {
   return (
     <div
@@ -65,6 +96,10 @@ function renderOverlay(color) {
 }
 
 class Placeholder extends Component {
+  /**
+   * Placeholders are rendered in locations whenever an answer is not.
+   * Connected to react-dnd higher order component 'DropTarget'.
+   */
   render() {
     const {
       connectDropTarget,
@@ -73,6 +108,7 @@ class Placeholder extends Component {
       toIndex,
     } = this.props;
 
+    //connectDropTarget is react-dnd function.
     return connectDropTarget(
       <div className="placeholder-container my-4 d-flex align-items-center">
         <h1 className="placeholder-container__number ml-4">{ toIndex + 1 }</h1>
@@ -84,14 +120,23 @@ class Placeholder extends Component {
 }
 
 Placeholder.propTypes = {
+  /** Supplied by react-dnd. */
   connectDropTarget: PropTypes.func.isRequired,
+  /** Supplied by react-dnd. True if drag item is hovering over placeholder */
   isOver: PropTypes.bool.isRequired,
+  /** Supplied by react-dnd. True for all Placeholders */
   canDrop: PropTypes.bool.isRequired,
+  /** Index of placeholder. Used if answer is 'dropped'. */
   toIndex: PropTypes.number.isRequired,
+  /** Old answer index. Used if answer is 'dropped'. */
   fromIndex: PropTypes.number.isRequired,
+  /** Changes position of answer to current placeholder position. Fed up to App component. */
   positionAnswer: PropTypes.func.isRequired,
+  /** Active question umber. Used if answer is 'dropped'. */
   activeQuestion: PropTypes.objectOf(PropTypes.any).isRequired,
+  /** side of old answer. Used if answer is 'dropped'. */
   fromSide: PropTypes.string.isRequired,
+  /** side of placeholder. Used if answer is 'dropped'. */
   toSide: PropTypes.string.isRequired,
 };
 
