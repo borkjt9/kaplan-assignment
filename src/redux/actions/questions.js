533fde07561bc380c57239140926ef37ad7c872b
borkjt9 <borkjt9@gmail.com>
2018-06-11 19:08:17 -0400

    Added redux documentation

--- src/redux/actions/questions.js
+++ src/redux/actions/questions.js
@@ -10,6 +10,16 @@ import {
 
 import { getQuestions, originalQuestionsDict } from '../../firebase/questions';
 
+/**
+ * Submits new position for answer
+ *
+ * @param {Object} data - Contains all info necessary to position answer to new location
+ * @key {Number} toIndex - new index position of answer.
+ * @key {Number} fromIndex - old index position of answer.
+ * @key {Number} activeQuestion - number reference for current question.
+ * @key {String} toSide - new side of answer. Either 'left' or 'right'.
+ * @key {String} fromSide - old side of answer. Either'left' or 'right'.
+ */
 export function positionAnswer(data) {
   return ({
     type: POSITION_ANSWER,
@@ -17,6 +27,14 @@ export function positionAnswer(data) {
   });
 }
 
+/**
+ * When an Answer component is clicked or dragged, setActiveAnswer
+ * stores that answer as active in the redux store.
+ *
+ * @param {Object} activeAnswer - The new active answer.
+ * @key {String} side - The active side. Either 'left' or 'right'.
+ * @key {Number} index - The index of the active answer
+ */
 export function setActiveAnswer(activeAnswer, question) {
   return ({
     type: SET_ACTIVE_ANSWER,
@@ -24,6 +42,11 @@ export function setActiveAnswer(activeAnswer, question) {
   });
 }
 
+/**
+ * Sets active question
+ *
+ * @param {Number} newQuestion - The new active question.
+ */
 export function setActiveQuestion(newQuestion) {
   return ({
     type: SET_ACTIVE_QUESTION,
@@ -31,6 +54,11 @@ export function setActiveQuestion(newQuestion) {
   });
 }
 
+/**
+ * Called by reset button. Resets answer locations in question to initial state.
+ *
+ * @param {Number} activeQuestion - The active Question
+ */
 export function resetActiveQuestion(activeQuestion) {
   const copy = JSON.parse(JSON.stringify(originalQuestionsDict[activeQuestion]));
 
@@ -40,12 +68,21 @@ export function resetActiveQuestion(activeQuestion) {
   });
 }
 
+/**
+ * Called when firebase call to load questions starts.
+ *
+ */
 export function getQuestionsStart() {
   return {
     type: GET_QUESTIONS_START,
   };
 }
 
+/**
+ * Called when firebase call to load questions errors out.
+ *
+ * @param {Object} error - Error response returned by firebase is call is unsuccessful.
+ */
 export function getQuestionsErrored(error) {
   return {
     type: GET_QUESTIONS_ERRORED,
@@ -53,6 +90,31 @@ export function getQuestionsErrored(error) {
   };
 }
 
+/**
+ * Called when firebase call to load questions is successful.
+ *
+ * @param {Object} data - Data returned by loadQuestions().
+ * EXAMPLE: {
+ *         0: {
+ *           questionText: '',
+ *           answers: {
+ *             left: {
+ *               0: '',
+ *               1: '',
+ *               2: '',
+ *               3: '',
+ *               4: '',
+ *             },
+ *             right: {
+ *             },
+ *             totalAnswers: 5,
+ *             activeAnswer: { side: 'left', index: -1 },
+ *           },
+ *         },
+ *         activeQuestion: 0,
+ *         status: 'loading',
+ *       };
+ */
 export function getQuestionsFulfilled(data) {
   return {
     type: GET_QUESTIONS_FULFILLED,
@@ -60,6 +122,12 @@ export function getQuestionsFulfilled(data) {
   };
 }
 
+/**
+ * Called by components to load questions from Firebase.
+ * This function kicks off all other actions related to loading Questions from Firebase.
+ *
+ * @param {Function} dispatch - Added to firebase function can communicate with store.
+ */
 export function requestGetQuestions(dispatch) {
   getQuestions(dispatch);
 }
--- src/redux/reducers/questionsReducer.js
+++ src/redux/reducers/questionsReducer.js
@@ -8,19 +8,40 @@ import {
   GET_QUESTIONS_FULFILLED,
 } from '../constants';
 
+/**
+ * Initial State suplied to store.
+ * Used to kick off loading animation when questions are being loaded.
+ */
 const initialState = {
   status: 'loading',
   activeQuestion: -1,
 };
 
+/**
+ * Called if toSide not equal from Side.
+ *
+
+ * @param {Object} answersDict - Contains current locations of answers to be rendered.
+ * EXAMPLE: { left: {0: 'answer1', 1: 'answer2', 3: 'answer 4'}, right: {2: 'answer4', 4: 'answer5'}} }
+ * @param {String} toSide - new side of answer. Either 'left' or 'right'.
+ * @param {String} fromSide - old side of answer. Either'left' or 'right'
+ * @param {Number} toIndex - new index position of answer.
+ * @param {Number} fromIndex - old index position of answer.
+ */
+
 function moveAnswerHoriz(answersDict, toSide, fromSide, toIndex, fromIndex) {
-  const newAnswersDict = answersDict;
+  const newAnswersDict = JSON.parse(JSON.stringify(answersDict));
+  // Check if new location already contains an answer.
   if (newAnswersDict[toSide][toIndex] === undefined) {
+    // Move answer.
     newAnswersDict[toSide][toIndex] = newAnswersDict[fromSide][fromIndex];
+    // Keeps answer active after move.
     newAnswersDict.activeAnswer = { side: toSide, index: toIndex };
     delete newAnswersDict[fromSide][fromIndex];
     return newAnswersDict;
   }
+
+  // Below runs if new location already contains answer.
   const temp = answersDict[toSide][toIndex];
   newAnswersDict[toSide][toIndex] = newAnswersDict[fromSide][fromIndex];
   newAnswersDict.activeAnswer = { side: toSide, index: toIndex };
@@ -28,17 +49,31 @@ function moveAnswerHoriz(answersDict, toSide, fromSide, toIndex, fromIndex) {
   return newAnswersDict;
 }
 
+/**
+ * Called if toSide equals from Side.
+ *
 
+ * @param {Object} answersDict - Contains current locations of answers to be rendered.
+ * EXAMPLE: { left: {0: 'answer1', 1: 'answer2', 3: 'answer 4'}, right: {2: 'answer4', 4: 'answer5'}} }
+ * @param {String} toSide - new side of answer. Either 'left' or 'right'.
+ * @param {String} fromSide - old side of answer. Either'left' or 'right'
+ * @param {Number} toIndex - new index position of answer.
+ * @param {Number} fromIndex - old index position of answer.
+ */
 function moveAnswerVert(answersDict, toSide, fromSide, toIndex, fromIndex) {
   const newAnswersDict = JSON.parse(JSON.stringify(answersDict));
+  // Checks if toIndex is out of range
   if (toIndex < 0 || toIndex >= 5) {
     return newAnswersDict;
+    // Checks if new position is occupied.
   } else if (newAnswersDict[toSide][toIndex] === undefined) {
     newAnswersDict[toSide][toIndex] = newAnswersDict[fromSide][fromIndex];
     newAnswersDict.activeAnswer = { side: toSide, index: toIndex };
     delete newAnswersDict[fromSide][fromIndex];
     return newAnswersDict;
   }
+
+  // If position is occupied, loop through to find next open position.
   let currIndex = toIndex;
   if (toIndex < fromIndex) {
     while (newAnswersDict[toSide][currIndex] !== undefined) {
@@ -49,6 +84,7 @@ function moveAnswerVert(answersDict, toSide, fromSide, toIndex, fromIndex) {
       currIndex += 1;
     }
   }
+  // Checks if new position is in range.
   if (currIndex >= 0 && currIndex < 5) {
     newAnswersDict[toSide][currIndex] = newAnswersDict[fromSide][fromIndex];
     newAnswersDict.activeAnswer = { side: toSide, index: currIndex };
@@ -57,6 +93,17 @@ function moveAnswerVert(answersDict, toSide, fromSide, toIndex, fromIndex) {
   return newAnswersDict;
 }
 
+/**
+ * Calls function to move answer horizontally or vertically.
+ *
+
+ * @param {Object} answersDict - Contains current locations of answers to be rendered.
+ * EXAMPLE: { left: {0: 'answer1', 1: 'answer2', 3: 'answer 4'}, right: {2: 'answer4', 4: 'answer5'}} }
+ * @param {String} toSide - new side of answer. Either 'left' or 'right'.
+ * @param {String} fromSide - old side of answer. Either'left' or 'right'
+ * @param {Number} toIndex - new index position of answer.
+ * @param {Number} fromIndex - old index position of answer.
+ */
 function positionAnswer(answersDict, toSide, fromSide, toIndex, fromIndex) {
   if (toSide !== fromSide) {
     return moveAnswerHoriz(answersDict, toSide, fromSide, toIndex, fromIndex);
@@ -64,6 +111,13 @@ function positionAnswer(answersDict, toSide, fromSide, toIndex, fromIndex) {
   return moveAnswerVert(answersDict, toSide, fromSide, toIndex, fromIndex);
 }
 
+/**
+ * Calls function to move answer horizontally or vertically.
+ *
+
+ * @param {Object} state - Maintains state for entire application.
+ * @param {Object} action - New information to be loaded to sate found in payload.
+ */
 export default function questions(state = initialState, action) {
   const newState = JSON.parse(JSON.stringify(state));
   switch (action.type) {
